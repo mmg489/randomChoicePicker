674107a89a4f048cba0dbdf2a5cf1d1454534748
Magi Munoz <m_mg489@outlook.com>
2023-07-25 22:56:22 -0400

    tag creation logic

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 })
 
-function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim()
-    !== '').map(tag => tag.trim())
-    console.log(tags)
-}
+// function createTags(input) {
+//     const tags = input.split(',').filter(tag => tag.trim()
+//     !== '').map(tag => tag.trim())
+//     console.log(tags)
+// }
 
+
+
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
