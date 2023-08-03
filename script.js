98ca33dd30b6f33b73f1a08dc13605f879e61fb4
Magi Munoz <m_mg489@outlook.com>
2023-08-02 23:49:23 -0400

    fixing script and index elements

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
-
-// function createTags(input) {
-//     const tags = input.split(',').filter(tag => tag.trim()
-//     !== '').map(tag => tag.trim())
-//     console.log(tags)
-// }
-
-
-
 
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
